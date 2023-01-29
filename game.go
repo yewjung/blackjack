659a8e93e3f3e7e1df873292dfe3eb30a9bf06d0
yewjung <yewjung@gmail.com>
2023-01-29 15:32:43 +0800

    partially implemented draw, stand + broadcast

--- game.go
+++ game.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	"fmt"
+	"strconv"
+	"sync"
+)
+
 type Suit int
 
 const (
@@ -24,9 +30,88 @@ func (data *GameData) value() int {
 
 type Game struct {
 	Deck    []Card
-	Players []Player
+	Players []*Player
 	Turn    int
 }
 
-func (game *Game) DrawCard() {}
-func (game *Game) Stand()    {}
+func (game *Game) DrawCard() {
+	currentPlayer := game.Players[game.Turn]
+	gameData := &currentPlayer.GameData
+
+	card := game.Deck[len(game.Deck)-1]
+	game.Deck = game.Deck[:len(game.Deck)-1]
+
+	gameData.Hand = append(gameData.Hand, card)
+
+	game.broadcastCardOtherPlayers(card)
+}
+
+func broadcastCard(currentPlayer *Player, card Card, wg *sync.WaitGroup) {
+	// TODO: implement broadcardCard
+	wg.Done()
+}
+
+func (game *Game) broadcastCardOtherPlayers(card Card) {
+	wg := sync.WaitGroup{}
+	wg.Add(len(game.Players))
+	// broadcast to current player about his new card
+	currentPlayer := game.Players[game.Turn]
+	go broadcastCard(currentPlayer, card, &wg)
+
+	// broadcast to other players about current players new card
+	// (but most not tell them the card details)
+	for i := 0; i < len(game.Players); i++ {
+		if game.Turn == i {
+			continue
+		}
+		player := game.Players[i]
+		go func(player *Player) {
+			// TODO: send a proper response here
+			response := fmt.Sprintf("new card for player[%s]", strconv.Itoa(game.Turn))
+			player.Conn.WriteJSON(response)
+			wg.Done()
+		}(player)
+	}
+	wg.Wait()
+
+}
+func (game *Game) Stand() {
+	game.broadcastStand()
+	game.Turn += 1
+}
+
+func (game *Game) broadcastStand() {
+	wg := sync.WaitGroup{}
+	wg.Add(len(game.Players))
+
+	// broadcast to current player about his stand
+	currentPlayer := game.Players[game.Turn]
+	go broadcastStand(currentPlayer, &wg)
+
+	// broadcast to other players about current players stand
+	for i := 0; i < len(game.Players); i++ {
+		if game.Turn == i {
+			continue
+		}
+		player := game.Players[i]
+		go func(player *Player) {
+			// TODO: send a proper response here
+			response := fmt.Sprintf("player[%s] stands", strconv.Itoa(game.Turn))
+			player.Conn.WriteJSON(response)
+			wg.Done()
+		}(player)
+	}
+	wg.Wait()
+}
+
+func broadcastStand(currentPlayer *Player, wg *sync.WaitGroup) {
+	// TODO: implement this method
+	wg.Done()
+}
+
+func (game *Game) StartGame() {
+	// prepare deck by shuffling
+
+	// send two cards to each player
+
+}
